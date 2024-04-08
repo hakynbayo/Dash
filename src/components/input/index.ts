4588623a81abc24ce75f5b89e4b6568df8218495
hakynbayo <akinbayo.akinwande@decagon.dev>
2024-04-08 05:03:04 +0100

    feat: area finder build file

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -1,4 +1,3 @@
 export { default as Input } from './input';
 export { default as InputLabel } from './inputLabel';
-export { default as MultiLine } from './multiLine';
 export { default as InputSearch } from './inputSearch';
